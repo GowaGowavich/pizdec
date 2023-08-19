ef4de8f21b9156208f289ada8cf50188d736b011
Gowa <ovakule14@gmail.com>
2023-08-19 19:37:41 +0300

    Добавьте файлы проекта.

---
+++ pizdec/Plugin.cs
@@ -0,0 +1,43 @@
+﻿using Exiled.API.Enums;
+using Exiled.API.Features;
+using Exiled.Events.Extensions;
+using Exiled.Events.Handlers;
+using InventorySystem;
+using PlayerRoles;
+using PluginAPI.Roles;
+
+namespace pizdec
+{
+    public class Plugin: Plugin<Config>
+    {
+        public override string Name => "Бродкаст момент";
+        public override string Author => "Григорий Лепин";
+        public override string Prefix => "queres";
+        public override void OnEnabled()
+        {
+            base.OnEnabled();
+            Exiled.Events.Handlers.Player.Spawned += Player_Spawned;
+            Exiled.Events.Handlers.Warhead.Detonated += Warhead_Detonated;
+        }
+
+        private void Player_Spawned(Exiled.Events.EventArgs.Player.SpawnedEventArgs ev)
+        {
+            if (ev.Player.Role.Type is RoleTypeId.Scientist)
+            {
+                ev.Player.AddAmmo(AmmoType.Nato9, 100);
+                ev.Player.AddItem(ItemType.SCP500);
+                ev.Player.Health = 500;
+                Log.Info("hui");
+            }
+        }
+
+        private void Warhead_Detonated()
+        {
+
+            foreach(Exiled.API.Features.Player Player in Exiled.API.Features.Player.List)
+            {
+                Player.Kill("Умер от радиации");
+            }
+        }
+    }
+}
---
+++ pizdec/gasmask.cs
@@ -0,0 +1,31 @@
+﻿using CommandSystem;
+using Exiled.API.Extensions;
+using Exiled.API.Features;
+using Exiled.API.Features.Items;
+using Exiled.API.Features.Roles;
+using PlayerRoles;
+using System;
+using System.Linq;
+
+namespace pizdec
+{
+    [CommandHandler(typeof(ClientCommandHandler))]
+    internal class gasmask : ICommand
+    {
+        public string Command => "emp";
+
+        public string[] Aliases => new string[] { };
+
+        public string Description => "Отключи всё";
+
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            string name = string.Join(" ", arguments);
+            var pidor = Player.Get(sender);
+            pidor.DisplayNickname = name;
+            response = $"Имя изменилось на {name}";
+            return true;
+        }
+    }
+}
+
